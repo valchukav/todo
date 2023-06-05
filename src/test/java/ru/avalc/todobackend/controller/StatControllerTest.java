374f877c59963b73a849e86c87f53bc151135067
Alexei Valchuk <a.valchukav@gmail.com>
2023-06-05 21:00:06 +0300

    test: provide tests for Stat controller

---
+++ src/test/java/ru/avalc/todobackend/controller/StatControllerTest.java
@@ -0,0 +1,36 @@
+package ru.avalc.todobackend.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import ru.avalc.todobackend.entity.Stat;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static ru.avalc.todobackend.controller.util.TestUtil.readFromJsonMvcResult;
+
+/**
+ * @author Alexei Valchuk, 05.06.2023, email: a.valchukav@gmail.com
+ */
+
+@Sql(scripts = "classpath:bd/populate_db.sql")
+public class StatControllerTest extends AbstractControllerTest {
+
+    @Test
+    void testGet() throws Exception {
+        MvcResult result = mockMvc.perform(MockMvcRequestBuilders.get("/stat/get"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+                .andReturn();
+
+        Stat stat = readFromJsonMvcResult(result, Stat.class);
+
+        assertNotNull(stat);
+        assertTrue(stat.getCompletedTotal() >= 0);
+        assertTrue(stat.getUncompletedTotal() >= 0);
+    }
+}
